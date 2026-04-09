6b59b5c055b3c85eae966364c1069e33fba11a28
seancho6616 <129930818+seancho6616@users.noreply.github.com>
2026-04-09 20:15:10 +0900

    플레이어 스탯 관리

---
+++ Assets/Script/Stats/PlayerStats.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public enum StatType
+{
+    MaxHP, WallCount, Luck, Insight,
+    MoveSpeed, ObjectAttack, AttackSpeed, WallAttack,
+}
+
+public class PlayerStats : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
