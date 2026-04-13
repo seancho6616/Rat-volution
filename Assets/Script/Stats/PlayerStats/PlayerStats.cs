f4bc43557704004a6127133574f4d9332a7355a0
seancho6616 <129930818+seancho6616@users.noreply.github.com>
2026-04-13 17:31:48 +0900

    플레이어 데이터 뒤 로직

--- Assets/Script/Stats/PlayerStats/PlayerStats.cs
+++ Assets/Script/Stats/PlayerStats/PlayerStats.cs
@@ -8,6 +8,18 @@ public enum StatType
 
 public class PlayerStats : MonoBehaviour
 {
- 
-    
+    public PlayerBaseStatData baseData;
+    public RunBonus runBonus = new RunBonus();
+
+    public int finalMaxHP =>baseData.maxHP + runBonus.maxHP;
+    public int finalWallCount => baseData.wallCount + runBonus.wallCount;
+
+    public float finalMoveSpeed => baseData.moveSpeed + runBonus.moveSpeed;
+    public float finalAttackSpeed => baseData.attackSpeed + runBonus.attackSpeed;
+    public float finalObjectAttack => baseData.objectAttack + runBonus.objectAttack;
+    public float finalWallAttack => baseData.wallAttack + runBonus.wallAttack;
+    public float finalLuck => baseData.luck + runBonus.luck;
+    public float finalInsight => baseData.insight + runBonus.insight;
+
+
 }
---
+++ Assets/Script/Stats/PlayerStats/RunBonus.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class RunBonus
+{
+    public int maxHP;
+    public int wallCount;
+    public float moveSpeed;
+    public float attackSpeed;
+    public float objectAttack;
+    public float wallAttack;
+    public float luck;
+    public float insight;
+
+    public void Reset()
+    {
+        maxHP       = 0;
+        wallCount   = 0;
+        moveSpeed   = 0f;
+        attackSpeed = 0f;
+        objectAttack= 0f;
+        wallAttack  = 0f;
+        luck        = 0f;
+        insight     = 0f;
+    }
+}
