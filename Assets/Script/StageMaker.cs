fbd9cf2beb3d107a5bfa6ce0a97b0b74d83c6979
seancho6616 <129930818+seancho6616@users.noreply.github.com>
2026-04-16 00:07:57 +0900

    바닥 코드 수정

--- Assets/Script/StageMaker.cs
+++ Assets/Script/StageMaker.cs
@@ -1,12 +1,47 @@
+using Unity.Collections;
 using UnityEngine;
 
 public class StageMaker : StageData
 {
+    public static StageMaker Instance;
+    [SerializeField]
+    private Material groundMaterial;
+
+    protected override void Awake()
+    {
+        if(Instance ==null) Instance = this;
+        else    Destroy(gameObject);
+        base.Awake();
+    }
     void Start()
     {
         transform.localScale = new Vector3(
             finalGridSizeCount*baseData.gridSize, 
-            -4f,
+            transform.localScale.y,
             finalGridSizeCount * baseData.gridSize);
+        SetTiling();    
+    }
+    public void GridSizeUP(int level)
+    {
+        if(level %5 == 0)
+        {
+            finalGridSizeCount += baseData.gridSizeCountPerLevel;
+            finalObjGridSizeCount += baseData.gridSizeCountPerLevel;
+            SizeUP();
+        }
+    }
+
+    private void SizeUP()
+    {
+        transform.localScale = new Vector3(
+            finalGridSizeCount*baseData.gridSize, 
+            transform.localScale.y,
+            finalGridSizeCount * baseData.gridSize);
+        SetTiling();
+    }
+
+    private void SetTiling()
+    {
+        groundMaterial.mainTextureScale = new Vector2(finalGridSizeCount, finalGridSizeCount);
     }
 }
