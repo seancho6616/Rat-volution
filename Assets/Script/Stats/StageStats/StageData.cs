c5982ffe5df024336c607c2c9339410d90a70e06
seancho6616 <129930818+seancho6616@users.noreply.github.com>
2026-04-16 00:09:29 +0900

    바닥 데이터 수정

--- Assets/Script/Stats/StageStats/StageData.cs
+++ Assets/Script/Stats/StageStats/StageData.cs
@@ -3,23 +3,13 @@ using UnityEngine.EventSystems;
 
 public class StageData : MonoBehaviour
 {
-    public static StageData Instance;
     public StageBaseStats baseData;
     public int finalGridSizeCount;
     public int finalObjGridSizeCount;
-    void Awake()
+    protected virtual void Awake()
     {
-        if(Instance == null) Instance = this;
-        else Destroy(gameObject);
         finalGridSizeCount = baseData.gridSizeCount;
         finalObjGridSizeCount = baseData.objGridSizeCount;
     }
-    public void GridSizeUP(int level)
-    {
-        if(level /5 == 0)
-        {
-            finalGridSizeCount += baseData.gridSizeCountPerLevel;
-            finalObjGridSizeCount += baseData.gridSizeCountPerLevel;
-        }
-    }
+    
 }
